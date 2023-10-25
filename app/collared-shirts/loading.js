f4d8a4793d68a235b6ee83a6b146733e10acf7d9
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-10-25 16:40:41 +0200

    connecting to db

---
+++ app/collared-shirts/loading.js
@@ -0,0 +1,5 @@
+const loading = () => {
+  return <div>{/* <LoadingSkeleton /> */}</div>;
+};
+
+export default loading;
---
+++ app/create-shirt/page.js
@@ -0,0 +1,10 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+const page = () => {
+  return <div>CreateShirt</div>;
+};
+
+export default page;
