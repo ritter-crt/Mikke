ee6d773e96ca814eab4579fe8e45a01c034716e5
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-10-30 14:55:51 +0100

    product write up

---
+++ public/assets/shirts/index.js
@@ -0,0 +1,34 @@
+import Andrea_slim_1 from './Andrea_slim_1.jpeg';
+import Ira_1 from './Ira_1.jpg';
+import Ira_2 from './Ira_2.jpg';
+import Ira_3 from './Ira_3.jpg';
+import Lotti_1 from './Lotti_1.jpg';
+import Mirta_1 from './Mirta_1.jpg';
+import Mirta_2 from './Mirta_2.jpg';
+import Mirta_3 from './Mirta_3.jpg';
+import Mirta_4 from './Mirta_4.jpg';
+
+import Mirta_fringe_1 from './Mirta_fringe_1.jpg';
+import Mirta_fringe_2 from './Mirta_fringe_2.jpg';
+import Mirta_fringe_3 from './Mirta_fringe_3.jpg';
+import Mirta_fringe_4 from './Mirta_fringe_4.jpg';
+import Mirta_fringe_5 from './Mirta_fringe_5.jpg';
+import Sammi_1 from './Sammi_1.jpg';
+import Sammi_2 from './Sammi_2.jpg';
+import Sammi_3 from './Sammi_3.jpg';
+import not_found from './not_found.jpg';
+
+export { Andrea_slim_1 };
+export { Ira_1, Ira_2, Ira_3 };
+export { Lotti_1 };
+export { Mirta_1, Mirta_2, Mirta_3, Mirta_4 };
+export {
+  Mirta_fringe_1,
+  Mirta_fringe_2,
+  Mirta_fringe_3,
+  Mirta_fringe_4,
+  Mirta_fringe_5,
+};
+export { Sammi_1, Sammi_2, Sammi_3 };
+
+export { not_found };
